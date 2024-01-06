e2e5e7afc990bdffd30af3212df9eaed5fdc4a96
Maksha006 <58694109+Maksha006@users.noreply.github.com>
2024-01-06 16:06:45 +0100

    création fin service

--- src/main/java/be/helb/smakani/service/FlightService.java
+++ src/main/java/be/helb/smakani/service/FlightService.java
@@ -1,4 +1,16 @@
 package be.helb.smakani.service;
 
+import be.helb.smakani.dao.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FlightService {
+
+    private final FlightRepository flightRepository;
+
+    @Autowired
+    public FlightService(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
 }
